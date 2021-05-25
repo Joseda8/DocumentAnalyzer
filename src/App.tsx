7c2f664a497e0cdc35d66a69b4b48b5b1b4a8162
Arturo Cordoba <arturocv16@gmail.com>
2021-05-24 21:57:27 -0600

    Reading environment variables

--- src/App.tsx
+++ src/App.tsx
@@ -48,6 +48,7 @@ function App() {
               
             </Switch>
           </Router>
+          <p>APIURL: {window._env_.URLAPI}</p>
       </MuiThemeProvider>
   );
 
--- src/helpers/constants.tsx
+++ src/helpers/constants.tsx
@@ -1,5 +1,11 @@
+declare global {
+    interface Window {
+        _env_: any;
+    }
+  }
+
 export const AUTHAPI_URL = 'http://localhost/AuthAPI/';
-export const urlAPI = 'http://localhost/DocumentAnalyzerAPI/';
+export const urlAPI = window._env_.URLAPI;
 //export const urlAPI = 'http://localhost:39748/';
 
 export default null;
