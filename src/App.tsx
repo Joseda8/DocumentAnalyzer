a81c1edbf9b8b9c56a266b09cda8c588b33780a4
Joseda8 <jd.montoya.s8@gmail.com>
2021-05-31 18:35:39 -0600

    Home view removed

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import './App.css';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
-import Home from "./components/home/home";
-import LogIn from "./components/home/login/login";
-import SignUp from "./components/home/sign_up/sign_up";
+// import Home from "./components/home/home";
+// import LogIn from "./components/home/login/login";
+// import SignUp from "./components/home/sign_up/sign_up";
 import Files from "./components/files/index";
 
 const theme = createMuiTheme({
@@ -28,24 +28,8 @@ function App() {
       <MuiThemeProvider theme={theme}>
           <Router>
             <Switch>
-              <Route exact path="/">
-                <Home title="Sign in">
-                  <LogIn message="Sign in"></LogIn>
-                </Home>
-              </Route>
-              <Route exact path="/sign_up">
-                <Home title="Sign up">
-                  <SignUp message="Sign in"></SignUp>
-                </Home>
-              </Route>
+              <Route exact path="/" component={Files} />
               <Route exact path="/files" component={Files} />
-
-              <Route>
-                <Home title="Sign in">
-                  <LogIn message="Sign in"></LogIn>
-                </Home>
-              </Route>
-              
             </Switch>
           </Router>
       </MuiThemeProvider>
