e8a40b2bfe6c15f0d8178a278494cca20809d339
Joseda8 <jd.montoya.s8@gmail.com>
2021-06-01 20:03:56 -0600

    WEB_SOCKET URL now is in constants file

--- src/helpers/constants.tsx
+++ src/helpers/constants.tsx
@@ -1,3 +1,4 @@
 export const AUTHAPI_URL = 'http://localhost/AuthAPI/';
 export const urlAPI = 'http://localhost:39748/';
+export const WEB_SOCKET = 'ws://127.0.0.1:8765';
 //export const urlAPI = 'http://localhost:39748/';
